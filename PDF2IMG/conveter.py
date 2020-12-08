385fe16a5dbb7e06a7587f410d5e769ebc82267d
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-12-08 09:32:33 +0530

    converter written

---
+++ PDF2IMG/conveter.py
@@ -0,0 +1,23 @@
+import os 
+from pdf2image import convert_from_path 
+
+def convert(path):
+
+    print(path)
+    
+    try:
+        os.system("mkdir output_file")
+
+
+    pages = convert_from_path(path, 300)
+    print(pages)
+    
+    path = path.split("/")
+    pdf_file = path[-1][:-4]
+
+
+    
+    for page in pages:
+        
+        page.save("output_file/%s-page%d.jpg" % (pdf_file,pages.index(page)), "JPEG")
+
